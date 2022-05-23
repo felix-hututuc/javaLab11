87bdeea42480eaa82aa1e54d4b3dab434e0b71f6
Hututuc Felix <felix.hututuc@gmail.com>
2022-05-23 10:58:57 +0300

    homework done

--- src/main/java/com/fii/springTesting/friends/FriendsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fii.springTesting.friends;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriendsRepository extends JpaRepository<Friends, Integer> {
-}
